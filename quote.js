79e2c2a1b842adadd9750cc810cedd8a48135fad
rubyred-thorn <110918468+rubyred-thorn@users.noreply.github.com>
2022-08-09 21:20:39 +0600

    Create quote.js

---
+++ quote.js
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="https://www.brainyquote.com/link/quotear.js"></script>
+<small><i><a href="/feeds/art" target="_blank" rel="nofollow">more Art Quotes</a></i></small>
